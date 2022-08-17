ffbdd5a2dbba5ea2aabff38bc5b53b3ed0710d09
sibergold <kaynakca21@hotmail.com>
2022-08-17 16:23:06 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace for_loop{
+    class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Lütfen bir sayı giriniz:");
+            int sayac = Convert.ToInt32(Console.ReadLine());
+        for(int i=1; i<=sayac;i++){
+            if(i%2==1){
+                Console.WriteLine(i);
+            }}
+            int tekToplam=0;
+            int ciftToplam=0;
+            for(int i=1;i<=1000;i++){
+              if(i%2==1){
+               tekToplam+=i;
+            
+              }
+              else 
+              {
+                ciftToplam+=i;
+              }
+              Console.WriteLine("Tek Toplam:"+tekToplam);
+              Console.WriteLine("Çift Toplam:"+ciftToplam);
+            
+            }
+            for (int i=1;i<10;i++){
+                if(i==4)
+                break;
+                Console.WriteLine(i);
+            }
+            for (int i=1;i<10;i++){
+                if(i==4)
+                continue;
+                Console.WriteLine(i);
+            }
+        
+    }
+}
+
+}
